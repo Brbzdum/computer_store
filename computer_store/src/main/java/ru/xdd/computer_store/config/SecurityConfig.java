8a2b621880ac03706e2069b303a5c545b27a7bbe
Brbzdum <vladimirbandenkov@gmail.com>
2025-01-12 05:57:41 +0300

    проблемы с security config но уже что-то выводит

--- computer_store/src/main/java/ru/xdd/computer_store/config/SecurityConfig.java
+++ computer_store/src/main/java/ru/xdd/computer_store/config/SecurityConfig.java
@@ -3,64 +3,62 @@ package ru.xdd.computer_store.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.authentication.AuthenticationManager;
 import ru.xdd.computer_store.service.CustomUserDetailsService;
-
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
+
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/users/login", "/users/registration", "/users/activate/**", "/products/**", "/manufacturers/**", "/custom-error", "/403").permitAll()
-                        .anyRequest().permitAll()
-
+                        .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/users/**", "/products/**", "/manufacturers/**", "/login", "/registration", "/403").permitAll()
+                        .anyRequest().authenticated()
                 )
-
-
                 .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
+                        .loginPage("/users/login") // Путь к вашей кастомной странице логина
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true) // Куда перенаправлять после успешного логина
+                        .failureUrl("/users/login?error=true") // Куда перенаправлять при ошибке
                         .permitAll()
                 )
-
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
+                        .logoutSuccessUrl("/users/login?logout=true")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
                         .permitAll()
                 )
-                .exceptionHandling(exception -> exception.accessDeniedPage("/403")); // Страница ошибки доступа
+                .exceptionHandling(exception -> exception
+                        .accessDeniedPage("/403") // Страница ошибки 403
+                );
         return http.build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-
-        return authenticationManagerBuilder.build();
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        return builder.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(10); // 10 - степень сложности
     }
 }
-
